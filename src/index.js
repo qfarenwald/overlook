a8fe303d4a4820c76607ae613cf453dd540da8d9
Quinne Farenwald <q.farenwald@gmail.com>
2019-09-13 16:59:32 -0600

    Change vars to lets

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ Promise.all([
   .catch(err => console.log(err));
 
 const getDate = () => {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0');
-  var yyyy = today.getFullYear();
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear();
   today = `${yyyy}/${mm}/${dd}`;
   return today;
 };
